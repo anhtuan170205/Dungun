294b7c5709c640a870b76269f0f6b0100edc6067
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-02-23 21:24:14 +0700

    Player Idle And Aim Weapon

--- Assets/Scripts/Mics/Settings.cs
+++ Assets/Scripts/Mics/Settings.cs
@@ -11,6 +11,16 @@ public static class Settings
 
     #region ROOM SETTINGS
     public const int maxChildCorridors = 3;
+    #endregion
 
+    #region ANIMATOR PARAMETERS
+    public static int aimUp = Animator.StringToHash("aimUp");
+    public static int aimUpRight = Animator.StringToHash("aimUpRight");
+    public static int aimUpLeft = Animator.StringToHash("aimUpLeft");
+    public static int aimDown = Animator.StringToHash("aimDown");
+    public static int aimRight = Animator.StringToHash("aimRight");
+    public static int aimLeft = Animator.StringToHash("aimLeft");
+    public static int isMoving = Animator.StringToHash("isMoving");
+    public static int isIdle = Animator.StringToHash("isIdle");
     #endregion
 }
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+
 [RequireComponent(typeof(Health))]
+[RequireComponent(typeof(PlayerControl))]
+[RequireComponent(typeof(IdleEvent))]
+[RequireComponent(typeof(Idle))]
+[RequireComponent(typeof(AimWeaponEvent))]
+[RequireComponent(typeof(AimWeapon))]
+[RequireComponent(typeof(AnimatePlayer))]
 [RequireComponent(typeof(SortingGroup))]
 [RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(Animator))]
@@ -11,16 +18,21 @@ using UnityEngine.Rendering;
 [RequireComponent(typeof(PolygonCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 [DisallowMultipleComponent]
+
 public class Player : MonoBehaviour
 {
     [HideInInspector] public PlayerDetailsSO playerDetails;
     [HideInInspector] public Health health;
+    [HideInInspector] public IdleEvent idleEvent;
+    [HideInInspector] public AimWeaponEvent aimWeaponEvent;
     [HideInInspector] public SpriteRenderer spriteRenderer;
     [HideInInspector] public Animator animator;
 
     private void Awake()
     {
         health = GetComponent<Health>();
+        idleEvent = GetComponent<IdleEvent>();
+        aimWeaponEvent = GetComponent<AimWeaponEvent>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
     }
