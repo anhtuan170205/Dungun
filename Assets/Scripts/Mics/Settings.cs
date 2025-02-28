38d1903ab2e1fbdf41275f7163855bc5826fe9a6
anhtuan170205 <tuan.tran170205@hcmut.edu.vn>
2025-02-28 15:25:27 +0700

    Collision Matrix And Animator Speed

--- Assets/Scripts/Mics/Settings.cs
+++ Assets/Scripts/Mics/Settings.cs
@@ -32,6 +32,7 @@ public static class Settings
     public static int rollDown = Animator.StringToHash("rollDown");
     public static int rollRight = Animator.StringToHash("rollRight");
     public static int rollLeft = Animator.StringToHash("rollLeft");
+    public static float baseSpeedForPlayerAnimations = 8f;
 
     // Door
     public static int open = Animator.StringToHash("open");
